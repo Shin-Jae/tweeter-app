57fb5e4a4075991505ad5c024efc6e20b8a30677
Jae Shin <7jaeshin@gmail.com>
2022-07-08 21:03:58 -0400

    profile page set-up

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -1,8 +1,27 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 
 function ProfilePage() {
+    const { profileId } = useParams();
+
+    const users = useSelector((state) => state.search[profileId])
+
     return (
-        <div>Profile</div>
+        <div>
+            <div>
+                {users?.first_name} {users?.last_name}
+            </div>
+            <div>
+                <img src={`${users?.profile_img}`} alt='profile-img' className='user-profile-img' />
+            </div>
+            <div>
+                {users?.username}
+            </div>
+            <div>
+                {users?.bio}
+            </div>
+        </div >
     )
 }
 
