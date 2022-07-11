2d62cc94e2ee4c55fc1cd56d5f5459953727323a
Jae Shin <7jaeshin@gmail.com>
2022-07-11 12:18:00 -0400

    new database

--- app/models/user.py
+++ app/models/user.py
@@ -6,7 +6,6 @@ from flask_login import UserMixin
 
 follows = db.Table(
     'follows',
-    db.Model.metadata,
     db.Column('follower_id', db.Integer, db.ForeignKey(
         'users.id'), primary_key=True),
     db.Column('followed_id', db.Integer, db.ForeignKey(
