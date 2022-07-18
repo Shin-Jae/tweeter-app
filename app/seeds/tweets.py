93ad3de10920c99e3d3b2afae83591b25d783fdb
Jae Shin <7jaeshin@gmail.com>
2022-07-18 05:54:23 -0400

    ids

--- app/seeds/tweets.py
+++ app/seeds/tweets.py
@@ -69,7 +69,7 @@ def seed_tweets():
         updated_at=datetime.datetime.now(),
     )
     tweet10 = Tweet(
-        user_id=7,
+        user_id=5,
         content="uh I think I'm in the wrong place",
         created_at=datetime.datetime.now(),
         updated_at=datetime.datetime.now(),
